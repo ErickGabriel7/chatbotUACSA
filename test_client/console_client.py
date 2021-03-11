d6034b58fbfef95f5cb3fa9c2c8cf2904604555c
kelvin <kelvinmaia@hotmail.com>
2021-03-11 07:22:36 -0300

    código e o conteúdo separado

--- test_client/console_client.py
+++ test_client/console_client.py
@@ -1,10 +1,20 @@
-import chatbotUACSA.chat.processing as processing
+from chatbotUACSA.chat.isolarcodigo import load_intents, load_responses, evaluate_intent, respond_intent
+import random
 
 if __name__ == '__main__':
+    available_intents = load_intents()
+    available_responses = load_responses()
+
+    random.seed()  # Prepare random number generation
+
     print('Iniciando interação com o chatbot')
     print('Para sair digite \q')
     text = ''
     while text != '\q':
         text = input('Você: ')
-        response = processing.create_answer(text, 'username')
-        print('Resposta:', response)
+        intent = evaluate_intent(text, available_intents)
+        if intent is None:
+            print('puxa acho que entendi mais ainda não sei responder.\nAtualmente só posso ajudar com comprovantes,dispensas,estágio e desligamento. ')
+        else:
+            response = respond_intent(intent, available_responses)
+            print('Resposta:', response)
