3f70b966f023977a92c1aec4a15f3c0e6027ff5b
kelvin <kelvinmaia@hotmail.com>
2021-03-16 12:32:01 -0300

    atualização da função create_answer adição da intent tipo_de_estagio

--- chatbotUACSA/chat/isolarcodigo.py
+++ chatbotUACSA/chat/isolarcodigo.py
@@ -2,15 +2,23 @@ import yaml
 import random
 from chatbotUACSA.utils import clean_text
 
+
 def load_intents():
     with open('intents.yaml', encoding='utf-8') as file:
         return yaml.load(file, yaml.SafeLoader)
+
+
 def load_responses():
     with open('responses.yaml', encoding='utf-8') as file:
         return yaml.load(file, yaml.SafeLoader)
 
-def evaluate_intent(text, intents):
-  for intent, content in intents.items():
+
+intents = load_intents()
+responses = load_responses()
+
+
+def evaluate_intent(text):
+    for intent, content in intents.items():
         for utterance in content['utterances']:
             # If the utterance has a + symbols it means that it has multiple AND terms.
             # Otherwise, a simple check will suffice.
@@ -21,9 +29,10 @@ def evaluate_intent(text, intents):
             elif utterance in text:
                 return intent
 
-  return None
+    return None
 
-def respond_intent(command, responses):
+
+def respond_intent(command):
     result = None
 
     try:
@@ -34,19 +43,19 @@ def respond_intent(command, responses):
 
     return result
 
+
 if __name__ == '__main__':
     # Load intents and responses from files
-    available_intents = load_intents()
-    available_responses = load_responses()
 
     print('Olá, eu sou o bot da uacsa!')
 
     while True:
         text = clean_text(input('O que você deseja? '))
 
-        intent = evaluate_intent(text, available_intents)
+        intent = evaluate_intent(text)
         if intent is None:
-            print('puxa acho que entendi mais ainda não sei responder.\nAtualmente só posso ajudar com comprovantes de matrícula,declaração de vínculo,dispensas,estágio e desligamento. ')
+            print(
+                'puxa acho que entendi mais ainda não sei responder.\nAtualmente só posso ajudar com comprovantes de matrícula,declaração de vínculo,dispensas,estágio e desligamento. ')
         else:
-            command_result = respond_intent(intent, available_responses)
+            command_result = respond_intent(intent)
             print(command_result)
--- test_client/console_client.py
+++ test_client/console_client.py
@@ -1,20 +1,6 @@
-from chatbotUACSA.chat.isolarcodigo import load_intents, load_responses, evaluate_intent, respond_intent
-import random
+from chatbotUACSA.chat.processing import create_answer
 
-if __name__ == '__main__':
-    available_intents = load_intents()
-    available_responses = load_responses()
-
-    random.seed()  # Prepare random number generation
-
-    print('Iniciando interação com o chatbot')
-    print('Para sair digite \q')
-    text = ''
-    while text != '\q':
-        text = input('Você: ')
-        intent = evaluate_intent(text, available_intents)
-        if intent is None:
-            print('puxa acho que entendi mais ainda não sei responder.\nAtualmente só posso ajudar com comprovantes,dispensas,estágio e desligamento. ')
-        else:
-            response = respond_intent(intent, available_responses)
-            print('Resposta:', response)
+text = ""
+while text != 'sair':
+    text = input("você: ")
+    resposta = create_answer(text)
