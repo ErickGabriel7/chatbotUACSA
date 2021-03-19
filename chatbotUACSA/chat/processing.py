9a57c6fdb305959735412e80aa398c8e16832e42
ErickGabriel7 <46802935+ErickGabriel7@users.noreply.github.com>
2021-03-19 10:17:46 -0300

    Corrigindo função create_awnser (print > return)

--- chatbotUACSA/chat/processing.py
+++ chatbotUACSA/chat/processing.py
@@ -1,13 +1,13 @@
 from chatbotUACSA.utils import clean_text
-from chatbotUACSA.chat.data_loading import load_intents, load_responses, evaluate_intent, respond_intent
+from chatbotUACSA.chat.data_loading import evaluate_intent, respond_intent
 
 
 def create_answer(input_text):
     input_text = clean_text(input_text)
     ok = evaluate_intent(input_text)
     if ok is None:
-        print(
+        return (
             'puxa acho que entendi mais ainda não sei responder.\nAtualmente só posso ajudar com comprovantes,dispensas,estágio e desligamento. ')
     else:
         response = respond_intent(ok)
-        print('Resposta:', response)
+        return ('Resposta:', response)
