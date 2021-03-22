5bc8855d5a5fafe8d7c105cf280a14f6b2ca7485
ErickGabriel7 <46802935+ErickGabriel7@users.noreply.github.com>
2021-03-22 14:20:50 -0300

    Organizando as respostas em responses.yaml

--- chatbotUACSA/chat/processing.py
+++ chatbotUACSA/chat/processing.py
@@ -6,9 +6,9 @@ def create_answer(input_text):
     input_text = clean_text(input_text)
     ok = evaluate_intent(input_text)
     if ok is None:
-        return {'text': 'puxa acho que entendi mais ainda não sei '
-                        'responder.Atualmente só posso ajudar com '
-                        'comprovantes,dispensas,estágio e desligamento. '}
+        return {'text': 'Puxa acho que entendi mais ainda não sei '
+                        'responder. Atualmente só posso ajudar com '
+                        'comprovantes, dispensas, estágio e desligamento.'}
     else:
         response = respond_intent(ok)
         return response
