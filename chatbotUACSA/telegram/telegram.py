2c142361b30ad440820d09c74c09a5383bb8e82c
ErickGabriel7 <46802935+ErickGabriel7@users.noreply.github.com>
2021-03-29 12:36:57 -0300

    Creating function send_answer

--- chatbotUACSA/telegram/telegram.py
+++ chatbotUACSA/telegram/telegram.py
@@ -23,16 +23,7 @@ def receive_message():
     app.logger.info(f"Chegou uma nova mensagem: {body}")
 
     resposta = process_message(body)
-    try:
-        if resposta['images'] is not None:
-            resposta_imagem = resposta['images']
-            resposta_texto = resposta['text']
-            app.logger.info(f"Resposta (send_photo): {resposta}")
-            send_photo(resposta_imagem, body, caption=resposta_texto)
-    except KeyError or TypeError:
-        resposta = resposta['text']
-        app.logger.info(f"Resposta (send_text_message): {resposta}")
-        send_text_message(resposta, body)
+    send_answer(resposta, body)
 
     # falar para o telegram que tudo ocorreu bem
     return {'ok': True}
@@ -49,6 +40,71 @@ def webhook():
     else:
         app.logger.info('Wrong event type')
         return 'Wrong event type', 400
+
+
+def send_answer(resposta, body):
+    # Função que processa a resposta gerada e escolhe que tipo de função de envio usar.
+    global there_is_img
+    global there_is_text
+    global there_is_doc
+    try:
+        if resposta['text'] is not None:
+            there_is_text = True
+    except KeyError:
+        there_is_text = False
+    try:
+        if resposta['images'] is not None:
+            there_is_img = True
+    except KeyError:
+        there_is_img = False
+    try:
+        if resposta['docs'] is not None:
+            there_is_doc = True
+    except KeyError:
+        there_is_doc = False
+    if there_is_text:
+        if there_is_img:
+            if there_is_doc:
+                resposta_imagem = resposta['images']
+                resposta_texto = resposta['text']
+                resposta_documento = resposta['docs']
+                app.logger.info(f"Resposta (send_photo e send_document): {resposta}")
+                send_photo(resposta_imagem, body, caption=resposta_texto)
+                send_document(resposta_documento, body)
+
+            else:
+                resposta_imagem = resposta['images']
+                resposta_texto = resposta['text']
+                app.logger.info(f"Resposta (send_photo): {resposta}")
+                send_photo(resposta_imagem, body, caption=resposta_texto)
+        else:
+            if there_is_doc:
+                resposta_documento = resposta['docs']
+                resposta_texto = resposta['text']
+                app.logger.info(f"Resposta (send_document): {resposta}")
+                send_document(resposta_documento, body, caption=resposta_texto)
+            else:
+                resposta = resposta['text']
+                app.logger.info(f"Resposta (send_text_message): {resposta}")
+                send_text_message(resposta, body)
+    elif there_is_doc:
+        resposta_documento = resposta['docs']
+        app.logger.info(f"Resposta (send_document): {resposta}")
+        send_document(resposta_documento, body)
+    else:
+        if there_is_img:
+            if there_is_doc:
+                resposta_documento = resposta['docs']
+                resposta_imagem = resposta['images']
+                app.logger.info(f"Resposta (send_photo e send_document): {resposta}")
+                send_photo(resposta_imagem, body)
+                send_document(resposta_documento, body)
+            else:
+                resposta_imagem = resposta['images']
+                app.logger.info(f"Resposta (send_photo): {resposta}")
+                send_photo(resposta_imagem, body)
+
+    pass
 
 
 def process_message(body):
@@ -105,14 +161,14 @@ def send_document(document_adress, body, caption=None):
         print('\n [ENVIANDO IMAGEM]:', document_adress, '\n')
     else:
         if caption is None:
-            with open(document_adress, 'rb') as document:
+            with open(os.path.join(script_dir, document_adress), 'rb') as document:
                 endpoint = f"https://api.telegram.org/bot{BOT_TOKEN}/sendDocument"
                 params = {
                     "chat_id": body['message']['chat']['id']
                 }
                 get(endpoint, params, files={'document': document})
         else:
-            with open(document_adress, 'rb') as document:
+            with open(os.path.join(script_dir, document_adress), 'rb') as document:
                 endpoint = f"https://api.telegram.org/bot{BOT_TOKEN}/sendDocument"
                 params = {
                     "chat_id": body['message']['chat']['id'],
