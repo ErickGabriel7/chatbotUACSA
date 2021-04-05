75ccb69bac37ed8dbf8120275d8705c37dbbe035
jhcp <jhcp@cin.ufpe.br>
2021-04-05 13:12:32 -0300

    imprimir arquivos, para visualização nos testes

--- test_client/console_client.py
+++ test_client/console_client.py
@@ -4,4 +4,8 @@ text = ""
 while text != 'sair':
     text = input("você: ")
     resposta = create_answer(text)
-    print(resposta)
+    print(resposta['text'])
+    if 'images' in resposta:
+        print('  --IMAGEN(S):', resposta['images'])
+    if 'docs' in resposta:
+        print('  --ARQUIVO(S):', resposta['docs'])
